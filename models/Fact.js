9e3144a67a8c4e4b0a426a3951358fa0ed527cb4
Quin Eddy <qkeddy@gmail.com>
2022-04-26 00:13:19 -0400

    Upgraded mysql2 and sequelize to the latest releases. Added Fact.js and created table reletionships.

---
+++ models/Fact.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Create Period model
+class Fact extends Model {}
+
+// create fields/columns for Location model
+Fact.init(
+    {
+        trans_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        period_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: "periods",
+                key: "id",
+            },
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: "products",
+                key: "id",
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: "users",
+                key: "id",
+            },
+        },
+        units: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        tableName: "fact",
+        timestamps: false,
+        indexes: [
+            {
+                name: "period_id",
+                using: "BTREE",
+                fields: [{ name: "period_id" }],
+            },
+            {
+                name: "product_id",
+                using: "BTREE",
+                fields: [{ name: "product_id" }],
+            },
+            {
+                name: "user_id",
+                using: "BTREE",
+                fields: [{ name: "user_id" }],
+            },
+        ],
+    }
+);
+
+module.exports = Fact;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,36 @@
 const Period = require("./Period");
 const Product = require("./Product");
 const User = require("./User");
+const Fact = require("./Fact");
 
-// TODO - Fill in relationships
+Fact.belongsTo(Period, {
+    as: "period",
+    foreignKey: "period_id"
+});
+Period.hasMany(Fact, {
+    as: "facts",
+    foreignKey: "period_id"
+});
+Fact.belongsTo(Product, {
+    as: "product",
+    foreignKey: "product_id"
+});
+Product.hasMany(Fact, {
+    as: "facts",
+    foreignKey: "product_id"
+});
+Fact.belongsTo(User, {
+    as: "user",
+    foreignKey: "user_id"
+});
+User.hasMany(Fact, {
+    as: "facts",
+    foreignKey: "user_id"
+});
 
-module.exports = { Period, Product, User };
+module.exports = {
+    Period,
+    Product,
+    User,
+    Fact
+};
