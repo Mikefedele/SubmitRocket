d36207cb77b209e1493cf2965dd314b1c9c7c38a
Quin Eddy <qkeddy@gmail.com>
2022-04-26 16:49:15 -0400

    In the sessions.js, moved sess secret to .env file and added helpful comments. In the connection.js, added reference and branching logic to support JAWSDB. Updated a few comments.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
-//* Config template
+// Require supporting NPM modules
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+// If the environment points to JAWSDB_URL, then use else, use the credentials in the .env file
+
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
-    }
-);
+        port: 3306,
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
 
+// Export the sequelize object
 module.exports = sequelize;
