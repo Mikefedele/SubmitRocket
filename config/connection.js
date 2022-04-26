64b31dfdd7556698491498ef7125127812b4856d
Quin Eddy <qkeddy@gmail.com>
2022-04-26 16:53:36 -0400

    Aligned "PASSWORD" param with .env file.

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306,
