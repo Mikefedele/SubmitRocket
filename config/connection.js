b3447fcdf320bab4819d126aeb90920b09846afd
Quin Eddy <qkeddy@gmail.com>
2022-04-26 21:09:57 -0400

    Completed configuration of JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // If the environment points to JAWSDB_URL, then use else, use the credentials in the .env file
-
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
